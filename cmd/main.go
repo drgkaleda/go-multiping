c2bfcec0d4609a3c61d3b39689ce9a6534ad97fc
drgkaleda <antanas@syntropynet.com>
2022-10-24 12:23:34 +0300

    Use IPs from commandline or file

--- cmd/main.go
+++ cmd/main.go
@@ -1,31 +1,76 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"log"
 	"net/netip"
+	"os"
 
 	"github.com/drgkaleda/go-multiping"
 )
 
-func main() {
-	data := multiping.NewPingData()
+func doPing(data *multiping.PingData, count int) error {
 	mp, err := multiping.New(false)
 	if err != nil {
-		log.Println(err)
-		return
+		return err
 	}
 
-	data.Add(netip.MustParseAddr("1.1.1.1"))
-	data.Add(netip.MustParseAddr("8.8.8.8"))
-	data.Add(netip.MustParseAddr("4.4.4.4"))
-	// For unknown reasons this IP reports dups. Usefull for testing
-	data.Add(netip.MustParseAddr("74.3.163.56"))
-
-	for i := 0; i < 100; i++ {
+	for i := 0; i < count; i++ {
 		mp.Ping(data)
 
+		// Print results
 		data.Iterate(func(ip netip.Addr, val multiping.PingStats) {
 			log.Println(ip, val.Valid(), val.Latency(), val.Loss(), val.Duplicate())
 		})
 	}
+	return nil
+}
+
+func main() {
+	fileName := flag.String("f", "", "File with IP list")
+	count := flag.Int("c", 5, "Stop after sending count pings")
+
+	flag.Parse()
+	data := multiping.NewPingData()
+
+	if fileName != nil && len(*fileName) > 0 {
+		file, err := os.Open(*fileName)
+		if err != nil {
+			log.Println("Error reading file", err)
+			return
+		}
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+			ip, err := netip.ParseAddr(line)
+			if err != nil {
+				log.Println("Invalid IP", line, err)
+				continue
+			}
+
+			data.Add(ip)
+		}
+
+	} else {
+
+		if len(os.Args[1:]) == 0 {
+			// No IPs configured - ping self
+			data.Add(netip.MustParseAddr("127.0.0.1"))
+		} else {
+
+			for _, ipstr := range os.Args[1:] {
+				ip, err := netip.ParseAddr(ipstr)
+				if err != nil {
+					log.Println("Invalid IP", ipstr, err)
+					continue
+				}
+
+				data.Add(ip)
+			}
+		}
+	}
+
+	doPing(data, *count)
 }
